8ee50e977a60359c963287d2edf981e30504c38d
dkzinn98 <derykalcinodasilva@icloud.com>
2025-01-02 16:48:55 -0300

    for-of-commit

--- script-for.js
+++ script-for.js
@@ -3,12 +3,14 @@ let input = document.querySelector("input")
 
 const contacts = [
     {name: "Deryk", number: "(61) 98166-3662"},
-    {name: "Lucas", number: "(61) 98621-1628"},
-    {name: "Davi", number: "(61) 99652-2638"},
-    {name: "Deivin", number: "(61) 98232-2832"},
-    {name: "Derio", number: "(61) 99679-8762"},
-    {name: "Delio", number: "(61) 98155-4672"},
+    {name: "Lucas", number: "(43) 98621-1628"},
+    {name: "Davi", number: "(94) 99652-2638"},
+    {name: "Deivin", number: "(81) 98232-2832"},
+    {name: "Derio", number: "(64) 99679-8762"},
+    {name: "Delio", number: "(22) 98155-4672"},
 ]
+
+// Desta forma abaixo eu estou puxando com conceito FOR
 
 function search(){
     for (let i = 0; i < contacts.length; i++) {
@@ -21,3 +23,20 @@ function search(){
     }
 
 }}
+
+/* Desta forma abaixo eu estou puxando com conceito FOR OF 
+
+function search(){
+    for(const contact of contacts){
+        
+        if (input.value.toLowerCase() === contact.name.toLowerCase()){
+            p.innerHTML = `Contact found, Name:${contact.name}, Phone:${contact.number}`
+            break
+        } else {
+        p.innerHTML = "I couldn't find this contact, please try again."
+    }
+
+}}
+
+*/
+
